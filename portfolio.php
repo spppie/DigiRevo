6b7afe202e9d6dc050966013394fffb5ddf5eb64
spppie <s.visser@rcnheliomare.nl>
2025-06-28 18:46:41 +0200

    digirevo 0.1

---
+++ portfolio.php
@@ -0,0 +1,31 @@
+<?php
+    
+?>
+<!DOCTYPE html>
+<html lang="">
+    <head>
+        <meta charset="UTF-8">
+        <title>Portfolio S Visser</title>
+        <meta name="description" content="php base">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <link rel="stylesheet" href="css/stylesheet.css">
+        <script src="js/script.js" defer></script>
+        <script src="https://kit.fontawesome.com/9f77a6e6f4.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div id="container">
+            <main>
+                <p id="menu-header">PORTFOLIO_</p>
+                <menu>
+                    <section class="menu-section"><a href="">REA WORK</a></section>
+                    <section class="menu-section"><a href="">PERSONAL</a></section>
+                    <section class="menu-section"><a href="https://github.com/spppie" target="_blank" class="a-ext">GITHUB<i class="fa-solid fa-square-arrow-up-right fa-xs"></i></i></a></section>
+                    <!-- <section class="menu-section">section 4</section> -->
+                </menu>
+                <section id="main-section">
+                    <p>test section</p>
+                </section>
+            </main>
+        </div>
+    </body>
+</html>
