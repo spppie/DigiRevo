1f1a70eb642301631841fdcb7daeb2a20d4e7a24
spppie <s.visser@rcnheliomare.nl>
2025-07-10 14:32:44 +0200

    DigiRevo 0.6.1

--- views/REA-sub/REA-JavaScript.php
+++ views/REA-sub/REA-JavaScript.php
@@ -1,6 +1,6 @@
 <section>
     <h1>JavaScript</h1>
     <!-- implement when live ! -->
-    <p>For JavaScript i worked with <a href="" class="inline-link" target="_blank">JSON<i class="fa-solid fa-square-arrow-up-right fa-xs"></i></a>(<a href="https://github.com/spppie/REA_work/tree/main/JS/JSon%20opdracht%204" class="inline-link" target="_blank">repo<i class="fa-solid fa-square-arrow-up-right fa-xs"></i></a>)</p>
+    <p>For JavaScript i worked with <a href=".../php_req/JS/JSon-opdracht-4/index.html" class="inline-link" target="_blank">JSON<i class="fa-solid fa-square-arrow-up-right fa-xs"></i></a>(<a href="https://github.com/spppie/REA_work/tree/main/JS/JSon%20opdracht%204" class="inline-link" target="_blank">repo<i class="fa-solid fa-square-arrow-up-right fa-xs"></i></a>)</p>
     <p>And DOM which can be seen in my <a href="index.php?page=PERSONAL&sub-page=MARATHON" class="inline-link" target="_blank">MARATHON</a> project.</p>
 </section>
