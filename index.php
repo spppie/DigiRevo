3590a895bc3211e7c1c9e113282b57edf36169f9
spppie <s.visser@rcnheliomare.nl>
2025-09-12 10:10:49 +0200

    DigiRevo 0.7.2

--- index.php
+++ index.php
@@ -58,7 +58,7 @@
         <meta charset="UTF-8">
         <title>Portfolio S Visser</title>
         <meta name="description" content="php base">
-        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
         <link rel="stylesheet" href="css/stylesheet.css">
         <style>
             :root {
