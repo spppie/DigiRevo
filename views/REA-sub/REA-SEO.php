cb34a271c7b1ff03e165d8d100bfbe29200b2716
spppie <s.visser@rcnheliomare.nl>
2025-09-16 11:49:15 +0200

    grammar and link correction

--- views/REA-sub/REA-SEO.php
+++ views/REA-sub/REA-SEO.php
@@ -1,4 +1,5 @@
 <section>
     <h1>SEO</h1>
+    <p>WIP</p>
     <p>As SEO is if done right an aspect of WebDev that the user will never see, so for this i could show you many html pages with nothing to see or just link to the <a href="" class="inline-link" target="_blank">repo</a> with my SEO work, seperated into different types.</p>
 </section>
